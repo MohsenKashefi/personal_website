990686ec3cb2c62f47397aacd2d97e50f34cc2c1
Mohsen Kashefi <mohsenkashefi2016@yahoo.com>
2025-10-10 16:23:34 -0700

    docs: add comprehensive README and improve carousel button functionality

--- lib/pages/home/components/carousel_items.dart
+++ lib/pages/home/components/carousel_items.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:personalwebsite/models/carousel_item_model.dart';
